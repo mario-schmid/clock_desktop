1245c9d8c8b950e6079544a24c78dd4436a409fa
mario <mario-schmid@mein.gmx>
2023-09-20 07:56:16 +0200

    update

--- src/main/java/com/software/clock_desktop/Main.java
+++ src/main/java/com/software/clock_desktop/Main.java
@@ -1,19 +1,11 @@
 package com.software.clock_desktop;
 
 import javafx.application.Application;
-//import javafx.scene.Camera;
 import javafx.scene.Group;
-//import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
-//import javafx.scene.paint.PhongMaterial;
-//import javafx.scene.shape.Circle;
-//import javafx.scene.shape.Rectangle;
-//import javafx.scene.shape.Sphere;
-//import javafx.scene.transform.Rotate;
-//import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -27,17 +19,6 @@ public class Main extends Application {
     ImageView ivZeigerStd = new ImageView();
 
     double opacity = 0.4;
-
-    //ImageView ivSonne = new ImageView();
-
-    //Rotate rotation = new Rotate();
-
-    //public static final float WIDTH = 800;
-    //public static final float HEIGHT = 800;
-
-    //Sphere sphere = new Sphere(334);
-
-    //Transform rotateEarth = new Rotate(-0.25, Rotate.Y_AXIS);
 
     @Override
     public void start(Stage stage) {
@@ -130,98 +111,7 @@ public class Main extends Application {
         stage.setX(1440);
         stage.setY(600);
 
-        /*
-        Stage stageSun = new Stage();
-
-        Image Sonne = new Image("Sonne.png");
-        ivSonne.setImage(Sonne);
-
-        ivSonne.setLayoutX(410);
-        ivSonne.setLayoutY(820);
-
-        rotation.setPivotX(60);
-        rotation.setPivotY(-350);
-        ivSonne.getTransforms().add(rotation);
-
-        rotation.setAngle(15 * stdint + 0.25 * minint);
-
-        Group sunroot = new Group();
-
-        sunroot.getChildren().add(ivSonne);
-
-        stageSun.initStyle(StageStyle.TRANSPARENT);
-        Scene sceneSun = new Scene(sunroot, 940, 940);
-        sceneSun.setFill(Color.TRANSPARENT);
-
-        stageSun.setScene(sceneSun);
-
-        stageSun.setX(490);
-        stageSun.setY(70);
-        */
-
-        /*Stage stageEarth = new Stage();
-
-        PhongMaterial material = new PhongMaterial();
-        material.setDiffuseMap(new Image("8081_earthmap10k.png"));
-        sphere.setMaterial(material);
-
-        Transform zero = new Rotate(195.267 - (15 * stdint + 0.25 * minint), Rotate.Y_AXIS);
-        sphere.getTransforms().add(zero);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(-2.5);
-        rectangle.setY(-WIDTH/2);
-        rectangle.setWidth(5);
-        rectangle.setHeight(HEIGHT);
-        rectangle.setFill(Color.YELLOWGREEN);
-
-        Circle circleTop = new Circle(0, -HEIGHT/2+20, 20);
-        circleTop.setFill(Color.YELLOWGREEN);
-        Circle circleButtom = new Circle(0, +HEIGHT/2-20, 20);
-        circleButtom.setFill(Color.YELLOWGREEN);
-
-        Group group = new Group();
-        group.getChildren().add(sphere);
-        group.getChildren().add(rectangle);
-        group.getChildren().add(circleTop);
-        group.getChildren().add(circleButtom);
-
-        Camera camera = new PerspectiveCamera();
-
-        stageEarth.initStyle(StageStyle.TRANSPARENT);
-        Scene sceneEarth = new Scene(group, WIDTH, HEIGHT);
-        sceneEarth.setFill(Color.TRANSPARENT);
-        sceneEarth.setCamera(camera);
-
-        group.translateXProperty().set(WIDTH/2);
-        group.translateYProperty().set(HEIGHT/2);
-        group.translateZProperty().set(0);
-
-        stageEarth.setX((1920-WIDTH)/2);
-        stageEarth.setY((1080-HEIGHT)/2);
-
-        stageEarth.setScene(sceneEarth);*/
-
-        //Stage stageSunLine = new Stage();
-
-//        Group groupSunLine = new Group();
-//        groupSunLine.getChildren().add(rectangle);
-//        groupSunLine.getChildren().add(circleTop);
-//        groupSunLine.getChildren().add(circleButtom);
-
-//        stageSunLine.initStyle(StageStyle.TRANSPARENT);
-//        Scene sceneSunLine = new Scene(groupSunLine, WIDTH, HEIGHT);
-//        sceneSunLine.setFill(Color.TRANSPARENT);
-
-//        stageSunLine.setX((1920-WIDTH)/2);
-//        stageSunLine.setY((1080-HEIGHT)/2);
-//
-//        stageSunLine.setScene(sceneSunLine);
-
         stage.show();
-        //stageSun.show();
-        //stageEarth.show();
-        //stageSunLine.show();
 
         Timer TimerTime = new Timer();
         TimerTime.schedule(new timerTaskTime(), 0, 1000);
@@ -248,9 +138,6 @@ public class Main extends Application {
             if (sekint == 1) {
 
                 ivZeigerStd.setRotate(30 * stdint + 0.5 * minint);
-                //rotation.setAngle(15 * stdint + 0.25 * minint);
-
-                //sphere.getTransforms().add(rotateEarth);
             }
         }
     }
